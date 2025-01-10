302d2d4d8f3aeba3782ffc02dd918c5656fa4412
AmanKumarWeb19 <akgupta0524@gmail.com>
2025-01-10 17:45:00 +0530

    connect to mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
 const express = require("express")
+const { connection } = require("./configs/db")
+require("dotenv").config();
 
 const app = express()
+app.use(express.json());
 
-app.listen(8080,()=>{
-    console.log("Server is running at Port 8080")
-})
+app.get("/", (req, res) => {
+    res.send("HOMEPAGE");
+  });
+
+    
+app.listen(process.env.port, async () => {
+    try {
+      await connection;
+      console.log("Connected to DB");
+    } catch (err) {
+      console.log("Didnot Connect to db");
+    }
+    console.log(`running at ${process.env.port}`);
+  });
+  
